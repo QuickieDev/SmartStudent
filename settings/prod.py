01f4628c47e1eda06a625a90275750abe4c27a9e
Vassilis Papavassiliou <vpapavasil@gmail.com>
2018-09-01 23:14:33 +0300

    Added text

--- settings/prod.py
+++ settings/prod.py
@@ -12,8 +12,8 @@ SERVER_HOST = 'smart-student'
 WSGI_OPTS = {
     'server': 'gunicorn',
     'reloader': True,
-    'port': 80,
-    'host': '0.0.0.0'
+    'port': 8000,
+    'host': '127.0.0.0'
 }
 
 
